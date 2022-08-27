98317508a57e7860e182ee19008cf45a7f3aa74f
Ethan Heinlein <ethanMH2002@gmail.com>
2022-08-27 16:12:49 -0400

    Hotfix initial commit
    Fixed a bug where the match wasn't hitting the NFT. :(

--- src/Collectable.cs
+++ src/Collectable.cs
@@ -3,65 +3,65 @@ using System;
 
 public class Collectable : RigidBody
 {
-    private GameManager manager;
-    private AudioStreamPlayer audio;
-    private bool in_coin = false;
-    private bool collected = false;
-    private int coinAmt = 1;
+	private GameManager manager;
+	private AudioStreamPlayer audio;
+	private bool in_coin = false;
+	private bool collected = false;
+	private int coinAmt = 1;
 
-    private AnimationPlayer anim;
+	private AnimationPlayer anim;
 
-    public override void _Ready()
-    {
-        audio = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-        manager = GetParent<GameManager>();
-        anim = GetNode<AnimationPlayer>("AnimationPlayer");
-    }
+	public override void _Ready()
+	{
+		audio = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		manager = GetParent<GameManager>();
+		anim = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
 
-    public override void _Process(float delta)
-    {
-        if(in_coin && !collected)
-        {
-            if(Input.IsActionJustPressed("player_action1"))
-            {
-                audio.Play();
-                GameManager.can_throw_matches = true;
-                collected = true;
-                GetChild<Spatial>(0).Visible = false;
-                GravityScale = 0;
-                manager.addCoin(coinAmt);
-                // Play animation here
-                anim.Play("Plus One");
-            }
-        }
+	public override void _Process(float delta)
+	{
+		if(in_coin && !collected)
+		{
+			if(Input.IsActionJustPressed("player_action1"))
+			{
+				audio.Play();
+				GameManager.can_throw_matches = true;
+				collected = true;
+				GetChild<Spatial>(0).Visible = false;
+				GravityScale = 0;
+				manager.addCoin(coinAmt);
+				// Play animation here
+				anim.Play("Plus One");
+			}
+		}
 
-        // Remove after sound is done
-        if(collected && !anim.IsPlaying())
-        {
-            QueueFree();
-        }
-    }
+		// Remove after sound is done
+		if(collected && !anim.IsPlaying())
+		{
+			QueueFree();
+		}
+	}
 
-    public void setCoinAmt(int amt)
-    {
-        coinAmt = amt;
-        GetNode<Label3D>("Text").Text = "+" + coinAmt.ToString();
-    }
+	public void setCoinAmt(int amt)
+	{
+		coinAmt = amt;
+		GetNode<Label3D>("Text").Text = "+" + coinAmt.ToString();
+	}
 
-    private void _on_Area_mouse_entered()
-    {
-        in_coin = true;
-        GameManager.can_throw_matches = false;
-    }
+	private void _on_Area_mouse_entered()
+	{
+		in_coin = true;
+		GameManager.can_throw_matches = false;
+	}
 
-    private void _on_Area_mouse_exited()
-    {
-        in_coin = false;
-        GameManager.can_throw_matches = true;
-    }
+	private void _on_Area_mouse_exited()
+	{
+		in_coin = false;
+		GameManager.can_throw_matches = true;
+	}
 
-    private void _on_VisibilityNotifier_screen_exited()
-    {
-        QueueFree();
-    }
+	private void _on_VisibilityNotifier_screen_exited()
+	{
+		QueueFree();
+	}
 }
--- src/GameManager.cs
+++ src/GameManager.cs
@@ -3,181 +3,181 @@ using System;
 
 public class GameManager : Node
 {
-    // NFT Assets
-    [Export]
-    private PackedScene nft;
-    private NFT curNft;
-    private NFTGenerator block_chain;
+	// NFT Assets
+	[Export]
+	private PackedScene nft;
+	private NFT curNft;
+	private NFTGenerator block_chain;
 
-    // Coin collectibles
-    [Export]
-    private PackedScene coin;
-    private Vector3 COIN_START = new Vector3(-0.48f, 2f, -1.8f);
-    RandomNumberGenerator rng;
-    private const float COIN_DROP_TIME = 40f;
-    private float cur_coin_drop;
+	// Coin collectibles
+	[Export]
+	private PackedScene coin;
+	private Vector3 COIN_START = new Vector3(-0.48f, 2f, -1.8f);
+	RandomNumberGenerator rng;
+	private const float COIN_DROP_TIME = 40f;
+	private float cur_coin_drop;
 
-    // Shop things
-    private Spatial gpu_storage;
-    private int gpus_active = 0;
-    private int match_down = 0;
-    private int pump_amt = 0;
+	// Shop things
+	private Spatial gpu_storage;
+	private int gpus_active = 0;
+	private int match_down = 0;
+	private int pump_amt = 0;
 
-    // Shop contrains
-    public const int MAX_GPUS = 10;
-    public const int MAX_MATCHES = 5;
-    public const int MAX_PUMP = 5;
-    public const int MAX_MARKET = 10;
+	// Shop contrains
+	public const int MAX_GPUS = 10;
+	public const int MAX_MATCHES = 5;
+	public const int MAX_PUMP = 5;
+	public const int MAX_MARKET = 10;
 
-    private UI ui;
-    private PlayerScript player;
+	private UI ui;
+	private PlayerScript player;
 
-    bool has_run = false;
-    private Vector3 nft_pos = new Vector3(0, 0, -2.258f);
+	bool has_run = false;
+	private Vector3 nft_pos = new Vector3(0, 0, -2.258f);
 
-    public static bool can_throw_matches = true;
-    public static int nfts_destroyed = 0;
-    public static int coins_collected = 0;
+	public static bool can_throw_matches = true;
+	public static int nfts_destroyed = 0;
+	public static int coins_collected = 0;
 
-    public override void _Ready()
-    {
-        // Get object references
-        block_chain = GetTree().Root.GetNode<NFTGenerator>("NftCreator");
-        ui = GetNode<UI>("UI");
-        gpu_storage = GetNode<Spatial>("GPU PowerUps");
-        player = GetNode<PlayerScript>("Camera");
+	public override void _Ready()
+	{
+		// Get object references
+		block_chain = GetTree().Root.GetNode<NFTGenerator>("NftCreator");
+		ui = GetNode<UI>("UI");
+		gpu_storage = GetNode<Spatial>("GPU PowerUps");
+		player = GetNode<PlayerScript>("Camera");
 
-        // NFT Gen
-        rng = new RandomNumberGenerator();
-        rng.Randomize();
+		// NFT Gen
+		rng = new RandomNumberGenerator();
+		rng.Randomize();
 
-        cur_coin_drop = COIN_DROP_TIME;
-    }
+		cur_coin_drop = COIN_DROP_TIME;
+	}
 
-    private void _on_Timer_timeout()
-    {
-        spawnNewNFT(); // Spawn first NFT
-    }
+	private void _on_Timer_timeout()
+	{
+		spawnNewNFT(); // Spawn first NFT
+	}
 
-    public override void _Process(float delta)
-    {
-        if(curNft == null)
-        {
-            return;
-        }
+	public override void _Process(float delta)
+	{
+		if(curNft == null)
+		{
+			return;
+		}
 
-        // NFT Generation
-        if(NFTGenerator.DONE_LOADING && !has_run)
-        {
-            curNft.replaceTexture(block_chain.getNFTMaterial(0));
-            curNft.Visible = true;
-            has_run = true;
-            block_chain.dumpNFTs();
-        }
+		// NFT Generation
+		if(NFTGenerator.DONE_LOADING && !has_run)
+		{
+			curNft.replaceTexture(block_chain.getNFTMaterial(0));
+			curNft.Visible = true;
+			has_run = true;
+			block_chain.dumpNFTs();
+		}
 
-        // Coin drops
-        if(cur_coin_drop <= 0)
-        {
-            spawnCoin();
-            // Reset timer, with a little randomization
-            cur_coin_drop = COIN_DROP_TIME + rng.RandiRange(-6, 6);
-        }
-        else
-        {
-            cur_coin_drop -= 0.1f;
-        }
-    }
+		// Coin drops
+		if(cur_coin_drop <= 0)
+		{
+			spawnCoin();
+			// Reset timer, with a little randomization
+			cur_coin_drop = COIN_DROP_TIME + rng.RandiRange(-6, 6);
+		}
+		else
+		{
+			cur_coin_drop -= 0.1f;
+		}
+	}
 
-    private void _on_NFT_tree_exited()
-    {
-        spawnNewNFT();
-        nfts_destroyed++;
+	private void _on_NFT_tree_exited()
+	{
+		spawnNewNFT();
+		nfts_destroyed++;
 
-        if(nfts_destroyed % 5 == 0) // Free coin for every 5 NFTs burned
-        {
-            spawnCoin();
-        }
+		if(nfts_destroyed % 5 == 0) // Free coin for every 5 NFTs burned
+		{
+			spawnCoin();
+		}
 
-        // Update UI
-        ui.updateUI();
-    }
+		// Update UI
+		ui.updateUI();
+	}
 
-    public void addCoin(int amt)
-    {
-        coins_collected += amt;
+	public void addCoin(int amt)
+	{
+		coins_collected += amt;
 
-        // Update UI
-        ui.updateUI();
-    }
+		// Update UI
+		ui.updateUI();
+	}
 
-    private void spawnCoin()
-    {
-        float x_coor = rng.RandfRange(-1.3f, 1.3f);
-        float z_rot = rng.RandiRange(-77, 77);
+	private void spawnCoin()
+	{
+		float x_coor = rng.RandfRange(-1.3f, 1.3f);
+		float z_rot = rng.RandiRange(-77, 77);
 
-        Collectable new_coin = (Collectable) coin.Instance();
-        AddChild(new_coin);
-        new_coin.Translation = new Vector3(x_coor, COIN_START.y, COIN_START.z);
-        new_coin.GetChild<Spatial>(0).RotationDegrees = new Vector3(0, z_rot, 0);
+		Collectable new_coin = (Collectable) coin.Instance();
+		AddChild(new_coin);
+		new_coin.Translation = new Vector3(x_coor, COIN_START.y, COIN_START.z);
+		new_coin.GetChild<Spatial>(0).RotationDegrees = new Vector3(0, z_rot, 0);
 
-        // Set value based on upgrade
-        if(pump_amt > 0)
-        {
-            new_coin.setCoinAmt(rng.RandiRange(1, pump_amt + 1));
-        }
-    }
+		// Set value based on upgrade
+		if(pump_amt > 0)
+		{
+			new_coin.setCoinAmt(rng.RandiRange(1, pump_amt + 1));
+		}
+	}
 
-    private void spawnNewNFT()
-    {
-        block_chain.generateNFTs(1);
-        // TODO: Slide-in effects
-        NFT new_nft = (NFT) nft.Instance();
-        new_nft.Visible = false;
-        CallDeferred("add_child", new_nft);
-        new_nft.Connect("tree_exiting", this, "_on_NFT_tree_exited");
-        new_nft.Translation = nft_pos;
+	private void spawnNewNFT()
+	{
+		block_chain.generateNFTs(1);
+		// TODO: Slide-in effects
+		NFT new_nft = (NFT) nft.Instance();
+		new_nft.Visible = false;
+		CallDeferred("add_child", new_nft);
+		new_nft.Connect("tree_exiting", this, "_on_NFT_tree_exited");
+		new_nft.Translation = nft_pos;
 
-        curNft = new_nft;
-        has_run = false;
-    }
+		curNft = new_nft;
+		has_run = false;
+	}
 
-    public int getActiveGpus()
-    {
-        return gpus_active;
-    }
+	public int getActiveGpus()
+	{
+		return gpus_active;
+	}
 
-    public int getMatchCooldownAmt()
-    {
-        return match_down;
-    }
+	public int getMatchCooldownAmt()
+	{
+		return match_down;
+	}
 
-    public int getPumpAmt()
-    {
-        return pump_amt;
-    }
+	public int getPumpAmt()
+	{
+		return pump_amt;
+	}
 
-    public void activateGpu()
-    {
-        gpus_active++;
-        GPUScript gpu = gpu_storage.GetChild<GPUScript>(gpus_active - 1);
-        gpu.Visible = true;
-        gpu.Connect("gpu_timeout", this, "_add_Coin_From_GPU");
-    }
+	public void activateGpu()
+	{
+		gpus_active++;
+		GPUScript gpu = gpu_storage.GetChild<GPUScript>(gpus_active - 1);
+		gpu.Visible = true;
+		gpu.Connect("gpu_timeout", this, "_add_Coin_From_GPU");
+	}
 
-    public void reduceMatchAmt()
-    {
-        match_down++;
-        player.reduceMatchTime(1.2f);
-    }
+	public void reduceMatchAmt()
+	{
+		match_down++;
+		player.reduceMatchTime(1.2f);
+	}
 
-    public void addCoinVariety()
-    {
-        pump_amt++;
-    }
+	public void addCoinVariety()
+	{
+		pump_amt++;
+	}
 
-    private void _add_Coin_From_GPU()
-    {
-        coins_collected++;
-        ui.updateUI();
-    }
+	private void _add_Coin_From_GPU()
+	{
+		coins_collected++;
+		ui.updateUI();
+	}
 }
